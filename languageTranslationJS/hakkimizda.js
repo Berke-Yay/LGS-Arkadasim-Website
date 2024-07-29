b0edc215518799a3418aa209f5e0c8799186859f
Berke Yay <105660365+Berke-Yay@users.noreply.github.com>
2024-07-29 19:49:43 +0300

    Update hakkimizda.js

--- languageTranslationJS/hakkimizda.js
+++ languageTranslationJS/hakkimizda.js
@@ -41,15 +41,15 @@ function configurePageLanguage(language){
 
         //Changing the language of the title and info section
         introductoryTitle.innerText = "Hakkımızda";
-        introductoryP.innerText = "2021'de kurulmuş bir proje olan ​LGS Arkadaşım, Türkiye'nin dört bir yanından aldığı 8. sınıf öğrencilerine ücretsiz bir şekilde bire bir mentorluk hizmeti sunarak ve deneme pratiği kazandırarak öğrencilerin LGS yolculuklarında tam potansiyellerine ulaşmalarını sağlıyor.";
+        introductoryP.innerText = "2021'de kurulmuş bir proje olan ​LGS Arkadaşım, Türkiye'nin dört bir yanından aldığı 8. sınıf öğrencilerine bire bir mentorluk hizmeti sunarak ve deneme pratiği kazandırarak öğrencilerin LGS yolculuklarında tam potansiyellerine ulaşmalarını sağlıyor.";
 
         //Changing the language of what makes us different section
         differentSectionTitle.innerText = "Bizi diğer programlardan neler ayırır?";
-        var newDifferentSectionSpans = ["Kaliteli bir Ekip", "Mentörlük", "Denemeler", "Küçük Sınıflarda Dersler", "Tercih Sürecinde Destek"];
+        var newDifferentSectionSpans = ["Kaliteli bir Ekip", "Mentörlük", "Denemeler", "Bire Bir Özel Dersler", "Tercih Sürecinde Destek"];
         var newDifferentSectionPs = ["Ekibimizdeki mentörler; yapılan mülakatlar sonucunda Koç Lisesi, Galatasaray Lisesi, Robert Koleji gibi Türkiye'nin en seçkin liselerinden seçilir.",
         "Her öğrencinin kendi mentörü vardır. Mentörler, öğrencileriyle düzenli olarak iletişim kurup onların sorularını cevaplar, tavsiyelerde bulunur. Mentörler aynı zamanda öğrencilere dersleri dışında da yardımcı olur.",
         "Her hafta çeşitli yayınlardan deneme olunur ve mentörler öğrencilerine bu denemelerin kazanım analizlerine göre soru verir.",
-        "Her ders için ayrı bir sınıf açılır, öğrenciler yapamadıkları soruları atarlar ve Zoom'da küçük bir sınıf halinde o sınıftaki öğrencilerin soruları haftalık derslerde çözülür.",
+        "Öğrenciler, Türkiye'nin nitelikli liselerinde okuyan mentörlerimizden belirli bir ücret karşılığında özel ders alabilmektedir. Özel ders gelirleri, dezavantajlı öğrencilere kitap bağışı yapmak için kullanılacaktır.",
         "Mentörler öğrencilerine sınav bittikten sonra tercih döneminde de yardımcı olurlar, hangi liseyi seçecekleri konusunda yol gösterirler."];
         for(var i=0;i<differentSectionSpans.length;i++){
             differentSectionSpans[i].innerText = newDifferentSectionSpans[i];
@@ -81,15 +81,15 @@ function configurePageLanguage(language){
 
         //Changing the language of the title and info section
         introductoryTitle.innerText = "About Us";
-        introductoryP.innerText = "Established in 2021, LGS Arkadaşım (My LGS Friend) is a project that aims to help 8th graders all around Turkey to reach their full potentials in their LGS(High School Entrance Exam) journey via offering students a free of cost one-to-one mentorship and lots of exam practices.";
+        introductoryP.innerText = "Established in 2021, LGS Arkadaşım (My LGS Friend) is a project that aims to help 8th graders all around Turkey to reach their full potentials in their LGS(High School Entrance Exam) journey via offering students one-to-one mentorship and lots of exam practices.";
 
         //Changing the language of what makes us different section
         differentSectionTitle.innerText = "What Seperates Us from Other Programs?";
-        var newDifferentSectionSpans = ["A Qualified Team", "Mentorship", "Exams", "Lessons in Small Classes", "Support on Decision Period"];
+        var newDifferentSectionSpans = ["A Qualified Team", "Mentorship", "Exams", "Private Lessons", "Support on Decision Period"];
         var newDifferentSectionPs = ["Mentors in our team are selected after a careful process of interviews from outstanding high schools of Turkey such as Koç High School, Galatasaray High School and Robert College.",
         "Each student has their own mentor. Mentors contact their students on a regular basis, and they answer the students' questions, giving them some advice. Mentors also help their students outside of academics.",
         "There is an exam every week and the mentors give homework to their students based on the exam analysis.",
-        "A separate class is opened for each subject, the students send the questions they cannot do, and these questions are solved in a small class on Zoom in weekly lessons.",
+        "Students can take private lessons for a certain fee from our mentors studying in qualified high schools in Turkey. Private lesson income will be used to donate books to disadvantaged students.",
         "Mentors keep guiding their students after the exam, helping them to choose the right high school for their interests and goals."];
         for(var i=0;i<differentSectionSpans.length;i++){
             differentSectionSpans[i].innerText = newDifferentSectionSpans[i];
@@ -121,15 +121,15 @@ function configurePageLanguage(language){
 
         //Changing the language of the title and info section
         introductoryTitle.innerText = "Über Uns";
-        introductoryP.innerText = "LGS Arkadaşım (Mein LGS Freund) wurde 2021 initiiert und ist ein Projekt, das Achtklässlern in der ganzen Türkei helfen soll, ihr volles Potenzial in ihrer High School Aufnahmeprüfung zu erreichen, indem sie den Schülern eine kostenlose Mentorenschaft und viele Prüfungspraktiken anbieten.";
+        introductoryP.innerText = "LGS Arkadaşım (Mein LGS Freund) wurde 2021 initiiert und ist ein Projekt, das Achtklässlern in der ganzen Türkei helfen soll, ihr volles Potenzial in ihrer High School Aufnahmeprüfung zu erreichen, indem sie den Schülern Mentorenschaft und viele Prüfungspraktiken anbieten.";
 
         //Changing the language of what makes us different section
         differentSectionTitle.innerText = "Was unterscheidet uns von anderen Programmen?";
-        var newDifferentSectionSpans = ["Ein Qualifiziertes Team", "Mentorenschaft", "Prüfungen", "Unterricht in Kleinen Klassen", "Unterstützung bei der Beschlussfassung"];
+        var newDifferentSectionSpans = ["Ein Qualifiziertes Team", "Mentorenschaft", "Prüfungen", "Privatunterricht", "Unterstützung bei der Beschlussfassung"];
         var newDifferentSectionPs = ["Die Mentoren in unserem Team werden nach einem sorgfältigen Prozess von Interviews von herausragenden Schulen der Türkei wie Koç High School, Galatasaray High School und Robert College ausgewählt.",
         "Jeder Schüler hat seinen eigenen Mentor. Mentorinnen und Mentoren nehmen regelmäßig Kontakt zu ihren Studierenden auf, beantworten die Fragen der Studierenden und geben Tipps. Mentoren helfen ihren Studierenden auch außerhalb der Wissenschaft.",
         "Es gibt jede Woche eine Prüfung und die Mentoren geben ihren Schülern basierend auf der Prüfungsanalyse Hausaufgaben.",
-        "Für jedes Fach wird eine separate Klasse geöffnet, die Schüler senden die Fragen, die sie nicht tun können, und diese Fragen werden in einer kleinen Klasse auf Zoom in wöchentlichen Lektionen gelöst.",
+        "Schüler können gegen eine bestimmte Gebühr Privatunterricht bei unseren Mentoren nehmen, die an qualifizierten Gymnasien in der Türkei studieren. Die Einnahmen aus Privatstunden werden dazu verwendet, Bücher an benachteiligte Schüler zu spenden.",
         "Mentoren begleiten ihre Schüler nach der Prüfung und helfen ihnen, die richtige Schule für ihre Interessen und Ziele zu wählen."];
         for(var i=0;i<differentSectionSpans.length;i++){
             differentSectionSpans[i].innerText = newDifferentSectionSpans[i];
@@ -162,15 +162,15 @@ function configurePageLanguage(language){
 
         //Changing the language of the title and info section
         introductoryTitle.innerText = "Sobre Nosotros";
-        introductoryP.innerText = "Iniciado en 2021, LGS Arkadaşım (Mi LGS Amigo) es un proyecto que tiene como objetivo ayudar a los estudiantes de octavo grado de todo Turquía a alcanzar su máximo potencial en su examen de ingreso a la escuela secundaria a través de ofrecer a los estudiantes una tutoría gratuita y muchas prácticas de exámenes.";
+        introductoryP.innerText = "My LGS Friend, un proyecto establecido en 2021, permite a los estudiantes alcanzar su máximo potencial en su viaje LGS al brindar un servicio de tutoría individual y práctica de prueba a estudiantes de octavo grado de toda Turquía.";
 
         //Changing the language of what makes us different section
         differentSectionTitle.innerText = "Qué nos separa de otros programas?";
-        var newDifferentSectionSpans = ["Un Equipo Cualificado", "Mentoría", "Exámenes", "Lecciones en Clases Pequeñas", "Apoyo Sobre el Período de Decisión"];
+        var newDifferentSectionSpans = ["Un Equipo Cualificado", "Mentoría", "Exámenes", "Lección Privada", "Apoyo Sobre el Período de Decisión"];
         var newDifferentSectionPs = ["Los mentores de nuestro equipo son seleccionados después de un cuidadoso proceso de entrevistas de escuelas secundarias sobresalientes de Turquía como Koç Secundaria, Galatasaray Secundaria y Robert College",
         "Cada estudiante tiene su propio mentor. Los mentores contactan a sus estudiantes de forma regular, y responden a las preguntas de los estudiantes, dándoles algunos consejos. Los mentores también ayudan a sus estudiantes fuera de lo académico.",
         "Hay un examen cada semana y los mentores dan tarea a sus estudiantes en base al análisis del examen.",
-        "Se abre una clase separada para cada asignatura, los estudiantes envían preguntas difíciles, y estas preguntas se resuelven en una pequeña clase sobre Zoom en las lecciones semanales.",
+        "Los estudiantes pueden tomar lecciones privadas por una tarifa determinada con nuestros mentores que estudian en escuelas secundarias calificadas en Turquía. Los ingresos de las lecciones privadas se utilizarán para donar libros a estudiantes desfavorecidos.",
         "Los mentores siguen guiando a sus estudiantes después del examen, ayudándoles a elegir la escuela secundaria adecuada para sus intereses y metas."];
         for(var i=0;i<differentSectionSpans.length;i++){
             differentSectionSpans[i].innerText = newDifferentSectionSpans[i];
@@ -203,15 +203,15 @@ function configurePageLanguage(language){
 
         //Changing the language of the title and info section
         introductoryTitle.innerText = "Sur Nous";
-        introductoryP.innerText = "Créé en 2021, LGS Arkadaşım (Mon LGS Ami) est un projet qui vise à aider les élèves de 8e année de toute la Turquie à atteindre leur plein potentiel dans leur parcours LGS (Examen d’entrée au Secondaire) en offrant aux étudiants unun mentorat et beaucoup de pratiques d’examen.";
+        introductoryP.innerText = "LGS Arkadaşım, un projet créé en 2021, permet aux étudiants d'atteindre leur plein potentiel dans leur parcours LGS en fournissant un service de mentorat individuel et des essais pratiques aux élèves de 8e année de toute la Turquie.";
 
         //Changing the language of what makes us different section
         differentSectionTitle.innerText = "Qu’est-ce Qui Nous Sépare des Autres Programmes?";
-        var newDifferentSectionSpans = ["Une Equipe Qualifiée", "Mentorat", "Examens", "Leçons en Petites Classes", "Soutien à la Période de Décision"];
+        var newDifferentSectionSpans = ["Une Equipe Qualifiée", "Mentorat", "Examens", "Cours Privé", "Soutien à la Période de Décision"];
         var newDifferentSectionPs = ["Les mentors de notre équipe sont sélectionnés après un processus minutieux d’entretiens avec des lycées exceptionnels de Turquie tels que le lycée Koç, le lycée Galatasaray et le collège Robert.",
         "Chaque étudiant a son propre mentor. Les mentors communiquent régulièrement avec leurs étudiants et ils répondent aux questions des étudiants en leur donnant des conseils. Les mentors aident également leurs étudiants en dehors des matières académiques.",
         "Il y a un examen chaque semaine et les mentors donnent des devoirs à leurs étudiants en fonction de l’analyse de l’examen.",
-        "Une classe séparée est ouverte pour chaque sujet, les étudiants envoient les questions qu’ils ne peuvent pas faire, et ces questions sont résolues dans une petite classe sur Zoom dans les leçons hebdomadaires.",
+        "Les étudiants peuvent suivre des cours particuliers moyennant un certain montant auprès de nos mentors qui étudient dans des lycées qualifiés en Turquie. Les revenus des cours particuliers serviront à faire don de livres à des élèves défavorisés.",
         "Les mentors continuent de guider leurs élèves après l’examen, les aidant à choisir la bonne école secondaire pour leurs intérêts et leurs objectifs."];
         for(var i=0;i<differentSectionSpans.length;i++){
             differentSectionSpans[i].innerText = newDifferentSectionSpans[i];
